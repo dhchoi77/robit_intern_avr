79795598fc3386be7c5b6dbd6e2ea32d654e1884
dhchoi <dh.choi20070331@gmail.com>
2026-07-30 14:24:11 +0900

    Update main.c

--- Day2/Day02_Task02/main.c
+++ Day2/Day02_Task02/main.c
@@ -16,10 +16,10 @@ int print_lcd();
 void print_led();
 
 int main(void)
-{	DDRF =0x00;
-	DDRA = 0xFF;
-	ADMUX = 0x40;
-	ADCSRA = 0x87;
+{	DDRF =0x00;				//입력설정
+	DDRA = 0xFF;			//출력설정
+	ADMUX = 0x40;			//PF0의 전압 측정과 avcc를 기준으로 하겠다.
+	ADCSRA = 0x87;			//adc활성화 및 분주비 128로 설정
 	
 	SREG = 0x80;
 	
@@ -34,39 +34,40 @@ int main(void)
 }
 
 int print_lcd(){
-	unsigned int adcValue =0;
-	char adcValue_print[5];
+	unsigned int adcValue =0;		//adc값을 받아올 변수(0~1023)
+	char adcValue_print[5];			//읽어온 adc값을 lcd에 출력할 문자열
 	unsigned char chaneel =0x00;
 	ADMUX = 0x40 | chaneel;
 	ADCSRA |=0x40;
-	while((ADCSRA&0x10)==0);
+	while((ADCSRA&0x10)==0);		//변환완료될때까지 기다리기
 	adcValue =ADC;
-	sprintf(adcValue_print,"%-4d",adcValue);
+	sprintf(adcValue_print,"%-4d",adcValue);		
 	i2c_lcd_goto_XY(0,0);
-	i2c_lcd_write_string("21th CDH");
+	i2c_lcd_write_string("21th CDH");		//이니셜 출력
 	i2c_lcd_goto_XY(1,0);
-	i2c_lcd_write_string(adcValue_print);
+	i2c_lcd_write_string(adcValue_print);	//adc값 출력(0~1023)
 	i2c_lcd_goto_XY(1,7);
 	int adcValue_float=adcValue;
-	adcValue_float=adcValue_float*5/1023;
+	adcValue_float=adcValue_float*5/1023;	//adc값을 전압으로 변경하기
 	sprintf(adcValue_print,"%1d",adcValue_float);
-	i2c_lcd_write_string(adcValue_print);
+	i2c_lcd_write_string(adcValue_print); //출력하기(소수점 위)
 	i2c_lcd_goto_XY(1,8);
 	i2c_lcd_write_string(".");
 	i2c_lcd_goto_XY(1,9);
 	long adcValue_float1=adcValue;
-	adcValue_float1=adcValue_float1*5%1023;
-	adcValue_float1=adcValue_float1*100/1023;
+	adcValue_float1=adcValue_float1*5%1023;		//소수점 아래를 구하기 위해서 나머지 구하기
+	adcValue_float1=adcValue_float1*100/1023;	//나머지 값이 0~1022사이이기 때문에 0~99사이로 변환해주기
 	adcValue_float=adcValue_float1;
 	sprintf(adcValue_print,"%-2d",adcValue_float);
-	i2c_lcd_write_string(adcValue_print);
+	i2c_lcd_write_string(adcValue_print);		//출력하기 (소수점 아래)
 	_delay_ms(100);
 	
 	return ADC;
 }
+//전압에 따라 led 출력해주기
 void print_led(){
 	int adc=ADC;
-	adc/=127;
+	adc/=127;		//adc값을 0~8사이로 만들어 주기
 	switch (adc){
 		case 8:
 			PORTA=0x00;
