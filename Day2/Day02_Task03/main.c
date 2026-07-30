e8c224a916ad4bad345e0fbf85791ec828dd2570
dhchoi <dh.choi20070331@gmail.com>
2026-07-30 14:07:29 +0900

    Update main.c

--- Day2/Day02_Task03/main.c
+++ Day2/Day02_Task03/main.c
@@ -12,32 +12,32 @@
 #include "clcd.h"
 #include "i2c.h"
 
-int A=1;
-int B=1;
-char circulate[4]={'+','-','*','/'};
-int circul_count=0;
-int result=2;
-int decimal=0;
-char temp[6];
+int A=1;		//앞에 숫자
+int B=1;		//뒤에 숫자
+char circulate[4]={'+','-','*','/'};		//연산부호
+int circul_count=0;		//연산부호의 인덱스
+int result=2;		//결과값
+int decimal=0;		//결과값의 소수점
+char temp[6];		//숫자의 출력을 위한 숫자를 저장할 임시 문자열
 
 int main(void)
-{	DDRD = 0x00;
-	DDRE = 0x00;
-	PORTD = 0xFF;   // 풀업 활성화
+{	DDRD = 0x00;		//입력설정(PD2,3)
+	DDRE = 0x00;		//출력설정(PE0,1)
+	PORTD = 0xFF;   // 풀업 활성화  
 	PORTE = 0xFF;   // 풀업 활성화
-	EIMSK=0b00111100;
-	EICRA = 0xA0;
-	EICRB = 0b00001010;
+	EIMSK=0b00111100;	//INT4,5,2,3을 인터럽트 설정
+	EICRA = 0xA0;		//INT2,3을 falling edge로 설정
+	EICRB = 0b00001010;	//INT4,5를 falling edge로 설정
 	sei();
 	i2c_lcd_init();
     /* Replace with your application code */
     while (1) 
     {	
-		i2c_lcd_goto_XY(0,0);
-		sprintf(temp,"%-3d",A);
+		i2c_lcd_goto_XY(0,0);	//출력할 위치로 이동
+		sprintf(temp,"%-3d",A);	//lcd에 출력 형식이 문자열 밖에 없어서 자료 형태 바꾸기
 		i2c_lcd_write_string(temp);
 		i2c_lcd_goto_XY(0,3);
-		char op_str[2];
+		char op_str[2];		//lcd 출력에 문자열만 들어갈 수 있어서 연산부호를 담는 문자열
 		op_str[0] = circulate[circul_count];
 		op_str[1] = '\0';
 		i2c_lcd_write_string(op_str);
@@ -50,14 +50,14 @@ int main(void)
 		i2c_lcd_goto_XY(0,8);
 		sprintf(temp,"%-3d",result);
 		i2c_lcd_write_string(temp);
-		if (decimal!=0){
+		if (decimal!=0){		//소수점에 숫자가 들어있다면 "."과 소수점을 출력하기
 			i2c_lcd_goto_XY(0,11);
 			i2c_lcd_write_string(".");
 			i2c_lcd_goto_XY(0,12);
 			sprintf(temp,"%-3d",decimal);
 			i2c_lcd_write_string(temp);
 		}
-		else{
+		else{					//소수점에 숫자가 0이라면 +,-,* 연산을 한 값이 들어있다는 것이기 때문에 lcd의 소수점이 출력되는 곳을 클리어 하기
 			i2c_lcd_goto_XY(0,11);
 			i2c_lcd_write_string("     ");
 		}
@@ -69,7 +69,7 @@ ISR(INT4_vect){
 	A++;
 }
 ISR(INT5_vect){
-	circul_count = (circul_count + 1) % 4;
+	circul_count = (circul_count + 1) % 4;		//count가 5이상으로 가면 문자열 범위를 벗어나므로 초기화도 같이 
 }
 ISR(INT2_vect){
 	B++;
@@ -88,7 +88,7 @@ ISR(INT3_vect){
 			result=A*B;
 			decimal=0;
 			break;
-		case '/':
+		case '/':		//소수점도 같이 구해 주기
 			result=A/B;
 			decimal=A%B;
 			break;
